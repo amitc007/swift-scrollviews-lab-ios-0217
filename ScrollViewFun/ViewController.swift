64190de2fa606bc18e9402589a210b5be92ddff8
ac <ac@Jaxxs-MacBook-Pro.local>
2017-02-27 14:49:03 -0500

    Completed

--- ScrollViewFun/ViewController.swift
+++ ScrollViewFun/ViewController.swift
@@ -9,11 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var sadnessImage: UIImageView!
+    @IBOutlet weak var joyImage: UIImageView!
+    @IBOutlet weak var fearImage: UIImageView!
+    @IBOutlet weak var disgustImage: UIImageView!
+    @IBOutlet weak var angerImage: UIImageView!
+ 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        sadnessImage.image = #imageLiteral(resourceName: "Sadness")
+        joyImage.image = #imageLiteral(resourceName: "Joy")
+        fearImage.image = #imageLiteral(resourceName: "Fear")
+        disgustImage.image = #imageLiteral(resourceName: "Disgust")
+        angerImage.image = #imageLiteral(resourceName: "Anger")
         
     }
 
